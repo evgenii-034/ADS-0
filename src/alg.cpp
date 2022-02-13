a5d3384c98087f0c64df8b8b483671222cd90e31
evgenii-034 <99119985+evgenii-034@users.noreply.github.com>
2022-02-13 16:45:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,26 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include "alg.h"
+using namespace std;
 
-int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+int gcd(int a, int b)
+{
+    while (true)
+    {
+        a = a % b;
+        if (a == 0)
+        {
+            return b;
+        }
+        b = b % a;
+        if (b == 0)
+        {
+            return a;
+        }
+    }
+}
+
+int main() {
+    cout << gcd(20, 60) << endl;
+    return 0;
 }
